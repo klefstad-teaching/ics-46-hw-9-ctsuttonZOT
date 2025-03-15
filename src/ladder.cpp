1049dc18c0d998ef7bceeab9a98dc8c8451920e9
ctsuttonZOT <ctracysutton@gmail.com>
2025-03-15 04:35:45 +0000

    Reverted edit_distance_within back to what it was before!

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -6,34 +6,29 @@ void error(string word1, string word2, string msg) {std::cerr << word1 << ' ' <<
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
     int m = str1.length();
-    int n = str2.length();
-
-    // if diff in length is larger than threshold, must be false
-    if (abs(n-m) > d) return false;
-
-    int prev; 
-    vector<int> curr(n + 1, 0); 
-
-    for (int j = 0; j <= n; j++)
-        curr[j] = j;
-  
-    for (int i = 1; i <= m; i++) {
-        prev = curr[0]; 
-        curr[0] = i;
-        int min_in_row = curr[0];
-        for (int j = 1; j <= n; j++) {
-            int temp = curr[j];
-            if (str1[i - 1] == str2[j - 1])
-                curr[j] = prev;
-            else
-                curr[j] = 1 + min({curr[j - 1], prev, curr[j]});
-            prev = temp;
-            min_in_row = min(min_in_row, curr[j]);
-        }
-        if (min_in_row > d) return false;
-    }
-    
-    return curr[n] <= d;
+     int n = str2.length();
+ 
+     // if diff in length is larger than threshold, must be false
+     if (abs(n-m) > d) return false;
+ 
+     // make a table to store results of sub-problems
+     vector<vector<int>> results(m + 1, vector<int>(n + 1));
+ 
+     for (int i = 0; i <= m; i++) 
+         results[i][0] = i;
+     for (int j = 0; j <= n; j++) 
+         results[0][j] = j; 
+ 
+     for (int i = 1; i <= m; i++) {
+         for (int j = 1; j <= n; j++) {
+             if (str1[i - 1] == str2[j - 1])
+                 results[i][j] = results[i - 1][j - 1];
+             else
+                 results[i][j] = 1 + min({results[i][j - 1], results[i - 1][j], results[i - 1][j - 1]});
+         }
+     }
+ 
+     return results[m][n] <= d;
 }
 
 bool is_adjacent(const string& word1, const string& word2) {return edit_distance_within(word1, word2, 1);}
