d541780766760f0cd0ee4fcfb41aedc0bc10024e
ctsuttonZOT <ctracysutton@gmail.com>
2025-03-15 05:15:16 +0000

    Added another break condition if target word not valid

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -6,35 +6,37 @@ void error(string word1, string word2, string msg) {std::cerr << word1 << ' ' <<
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
     int m = str1.length();
-     int n = str2.length();
- 
-     // if diff in length is larger than threshold, must be false
-     if (abs(n-m) > d) return false;
- 
-     // make a table to store results of sub-problems
-     vector<vector<int>> results(m + 1, vector<int>(n + 1));
- 
-     for (int i = 0; i <= m; i++) 
-         results[i][0] = i;
-     for (int j = 0; j <= n; j++) 
-         results[0][j] = j; 
- 
-     for (int i = 1; i <= m; i++) {
-         for (int j = 1; j <= n; j++) {
-             if (str1[i - 1] == str2[j - 1])
-                 results[i][j] = results[i - 1][j - 1];
-             else
-                 results[i][j] = 1 + min({results[i][j - 1], results[i - 1][j], results[i - 1][j - 1]});
-         }
-     }
- 
-     return results[m][n] <= d;
+    int n = str2.length();
+
+    // if diff in length is larger than threshold, must be false
+    if (abs(n-m) > d) return false;
+
+    // make a table to store results of sub-problems
+    vector<vector<int>> results(m + 1, vector<int>(n + 1));
+
+    for (int i = 0; i <= m; i++) 
+        results[i][0] = i;
+    for (int j = 0; j <= n; j++) 
+        results[0][j] = j; 
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (str1[i - 1] == str2[j - 1])
+                results[i][j] = results[i - 1][j - 1];
+            else
+                results[i][j] = 1 + min({results[i][j - 1], results[i - 1][j], results[i - 1][j - 1]});
+        }
+    }
+
+    return results[m][n] <= d;
 }
 
 bool is_adjacent(const string& word1, const string& word2) {return edit_distance_within(word1, word2, 1);}
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    // if target word not a word in dict or target word = input word, return empty ladder
     if (begin_word == end_word) return {};
+    if (word_list.find(end_word) == word_list.end()) return {};
     std::queue<std::vector<std::string>> ladder_queue;
     ladder_queue.push({begin_word});
     std::unordered_set<std::string> visited;
